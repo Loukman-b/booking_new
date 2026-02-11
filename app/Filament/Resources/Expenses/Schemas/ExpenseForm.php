94c249986f63d854052f4b3334ea697fe6faef3b
Loukman Bouhtala <B.loukman2004@gmail.com>
2026-02-11 14:05:46 +0100

    De entiteit aangemaakt die verantwoordelijk is voor de uitgaven. Hiermee kan de admin voor zijn eigen overzicht zijn uitgaven registreren

---
+++ app/Filament/Resources/Expenses/Schemas/ExpenseForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Expenses\Schemas;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ExpenseForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('description')
+                    ->required(),
+                TextInput::make('amount')
+                    ->required()
+                    ->numeric(),
+                DatePicker::make('expense_date')
+                    ->required(),
+                TextInput::make('created_by')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
