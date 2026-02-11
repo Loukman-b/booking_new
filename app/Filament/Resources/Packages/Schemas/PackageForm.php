d3751b7bb88e19951ea97a26c4b02ab0fc2d9a68
Loukman Bouhtala <B.loukman2004@gmail.com>
2026-02-11 14:07:47 +0100

    Hiermee kan de admin pakketjes beheeren die later in de home pagina terecht komen. Klanten moeten later een van dit pakketjes kiezen om een afspraak te maken

---
+++ app/Filament/Resources/Packages/Schemas/PackageForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\Packages\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class PackageForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                TextInput::make('duration_minutes')
+                    ->required()
+                    ->numeric(),
+                Toggle::make('active')
+                    ->required(),
+            ]);
+    }
+}
